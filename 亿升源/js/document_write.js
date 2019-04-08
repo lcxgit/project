cb2bd5dffa06a5c72820a5b3cb6ef4cd4979816a
lcxgit <1243878247@qq.com>
2019-04-08 18:09:45 +0800

    first commit

---
+++ 亿升源/js/document_write.js
@@ -0,0 +1,56 @@
+﻿
+/*Plugin library*/
+
+/*jquery*/
+document.write("<scr"+"ipt src='js/jquery.2.2.4.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+/*jquery*/
+
+/*bootstrap*/
+document.write("<scr"+"ipt src='js/bootstrap.3.3.7.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+/*bootstrap*/
+
+/*wow*/
+document.write("<scr"+"ipt src='js/wow.1.1.3.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+/*wow*/
+
+/*swiper*/
+//document.write("<scr"+"ipt src='js/swiper.4.5.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+/*swiper*/
+
+/*owl*/
+document.write("<scr"+"ipt src='js/owl.carousel.2.3.4.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+/*owl*/
+
+/*幻灯片切换动画*/
+//document.write("<scr"+"ipt src='js/jquery.flexslider.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+/*幻灯片切换动画*/
+
+/*点击图片放大*/
+//document.write("<scr"+"ipt src='js/gallery.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+document.write("<scr"+"ipt src='js/simple.slide.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+/*点击图片放大*/
+
+/*数字输入框*/
+//document.write("<scr"+"ipt src='js/jquery.input-counter.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+/*数字输入框*/
+
+/*禁止Ctrl+鼠标滚轴浏览器页面缩放*/
+// 方法 1
+//document.write("<scr"+"ipt src='js/smooth.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+
+// 方法 2
+//document.write("<scr"+"ipt src='js/onmousewheel.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+/*禁止Ctrl+鼠标滚轴浏览器页面缩放*/
+
+/*图片无缝滚动*/
+document.write("<scr"+"ipt src='js/jquery.SuperSlide.2.1.3.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+/*图片无缝滚动*/
+
+/*Plugin library*/
+
+document.write("<scr"+"ipt src='js/public.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+
+/*kefu*/
+//document.write("<scr"+"ipt src='js/customer_service.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+/*kefu*/
+
