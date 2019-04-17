424337ce1bedf3a6d221a5d1cc734690ea13864d
lcxgit <1243878247@qq.com>
2019-04-17 17:28:07 +0800

    first commit

---
+++ 维客/js/map.js
@@ -0,0 +1,5 @@
+﻿
+/*map*/ 
+document.write("<scr"+"ipt src='http://api.map.baidu.com/api?key=&v=1.2&services=true' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+document.write("<scr"+"ipt src='js/baiduMaps_api.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+/*map*/ 
