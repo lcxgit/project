02b49fcaf53602ea5b11d04c6a4b3d0a34b716e1
lcxgit <1243878247@qq.com>
2019-04-26 18:15:07 +0800

    删除

--- 华丰/js/map.js
+++
@@ -1,5 +0,0 @@
-﻿
-/*map*/ 
-document.write("<scr"+"ipt src='http://api.map.baidu.com/api?key=&v=1.2&services=true' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
-document.write("<scr"+"ipt src='js/baidu_map.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
-/*map*/ 
