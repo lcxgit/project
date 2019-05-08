b85b97e6ae5751d4d600c3a7316d9366e05884dc
lcxgit <1243878247@qq.com>
2019-05-08 17:43:24 +0800

    first commit

---
+++ 精一家具/js/document_write.js
@@ -0,0 +1,52 @@
+﻿
+/*Plugin library*/
+
+/*jquery*/
+document.write("<scr"+"ipt src='js/jquery.2.2.4.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+/*jquery*/
+
+/*wow*/
+document.write("<scr"+"ipt src='js/wow.min.1.0.2.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+/*wow*/
+
+/*bootstrap*/
+document.write("<scr"+"ipt src='js/bootstrap.3.3.7.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+// select 下拉菜单
+//document.write("<scr"+"ipt src='js/bootstrap-select.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+//下拉菜单
+//document.write("<scr"+"ipt src='js/bootstrap_dropdown_hover.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+/*bootstrap*/
+
+/*swiper*/
+document.write("<scr"+"ipt src='js/swiper.min.4.0.7.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+/*swiper*/
+
+/*owl*/
+document.write("<scr"+"ipt src='js/owl.carousel.2.3.4.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+/*owl*/
+
+/*幻灯片切换动画*/
+// document.write("<scr"+"ipt src='js/bootstrap-touch-slider.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+// document.write("<scr"+"ipt src='js/touchSwipe.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+/*幻灯片切换动画*/
+
+/*nav*/
+document.write("<scr"+"ipt src='js/nav.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+/*nav*/
+
+/*禁止Ctrl+鼠标滚轴浏览器页面缩放*/
+// 方法 1
+//document.write("<scr"+"ipt src='js/smooth.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+
+// 方法 2
+//document.write("<scr"+"ipt src='js/onmousewheel.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+/*禁止Ctrl+鼠标滚轴浏览器页面缩放*/
+
+/*Plugin library*/
+
+document.write("<scr"+"ipt src='js/public.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+
+/*kefu*/
+//document.write("<scr"+"ipt src='js/customer_service.js' type='text\/javascript' charset='utf-8'></scr"+"ipt>");
+/*kefu*/
+
